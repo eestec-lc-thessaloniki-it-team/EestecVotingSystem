e7c96d5c8d9891a3ad7a96ee743cdc6446bdf399
Zatrazemis <apostoliszatrazemis@gmail.com>
2025-07-02 21:33:02 +0300

    Nothing much

--- backend/src/main/java/com/eestecVotingSystem/votingsystem/controllers/dto/UserStripped.java
+++ backend/src/main/java/com/eestecVotingSystem/votingsystem/controllers/dto/UserStripped.java
@@ -1,4 +1,12 @@
 package com.eestecVotingSystem.votingsystem.controllers.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter // Setter and Getter BoilerPlate are handled by Lombok and the
+@Setter
+
 public class UserStripped {
+
 }
